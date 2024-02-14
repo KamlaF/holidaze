330941339c92634424e5d2124061f1cc5fb24981
Kamilla Fagerland <kamilla@askeladdenressurs.no>
2024-02-14 09:16:22 +0100

    Update navbar.js

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -18,7 +18,7 @@ const Navbar = () => {
         <Link to="/" className="font-bold">Holidaze</Link>
         <div className="md:hidden">
           <button onClick={() => setIsMenuOpen(!isMenuOpen)}>
-            {/* SVG-koden for Hamburger-ikon */}
+           
             <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7" />
             </svg>
