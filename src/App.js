a44b2f7b9ea6d0a22fe31441e17e1cbe2fdb64e1
Kamilla Fagerland <kamilla@askeladdenressurs.no>
2024-02-25 13:29:11 +0100

    fix
    import file update

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import {
   Navigate,
 } from "react-router-dom";
 import useAuthStore from "./store/authStore";
-import Layout from "./components/home/Layout";
+import Layout from "./components/Home/Layout";
 import RegistrationForm from "./components/LoginAndRegistration/RegistrationForm";
 import LoginForm from "./components/LoginAndRegistration/LoginForm";
-import Home from "./components/home/Home";
+import Home from "./components/Home/Home";
 import VenueDetail from "./components/Venues/VenueDetail";
 import UpdateVenueForm from "./components/Venues/UpdateVenueForm";
 import MyBookings from "./components/Bookings/MyBookings";
